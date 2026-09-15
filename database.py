17a4b8988a03c7da1b097ec11001ec45694c038f
yaeli <yaelilan2008@gmail.com>
2026-09-15 18:08:13 +0300

    fixed stuff

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 import sqlite3
 
-
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
+
+
 def start_soldiers_db():
     text = """CREATE TABLE IF NOT EXISTS GUESTS (
-    guest_id INT IDENTITY(1, 1) PRIMARY KEY,
+    guest_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     family_name TEXT,
     email TEXT UNIQUE,
@@ -13,19 +14,33 @@ def start_soldiers_db():
     )"""
     cursor.execute(text)
 
+
 def start_hosts_db():
     text = """CREATE TABLE IF NOT EXISTS HOSTS (
-    host_id INT IDENTITY(1,1) PRIMARY KEY,
+    host_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     family_name TEXT,
     email TEXT UNIQUE,
     password TEXT,
-    id INT,
+    id INTEGER,
     address TEXT,
-    phone_number INT
+    phone_number INTEGER
     )"""
     cursor.execute(text)
 
-def add_guest(name, family, email, password): #can be changed into an object class (simpler probably)
-    text = """INSTERT INTO GUESTS (name, family_name, email, password)
+
+def add_guest(name, family, email, password):  # can be changed into an object class (simpler probably)
+    text = """INSERT INTO GUESTS (name, family_name, email, password)
     VALUES (?,?,?,?)"""
+
+    cursor.execute(text, (name, family, email, password))
+    conn.commit()
+
+
+def add_host(name, family, email, password, id, address, phone):
+    text = """INSERT INTO HOSTS (name, family_name, email, password, id, address, phone_number)
+    VALUES (?,?,?,?,?,?,?)"""
+
+    cursor.execute(text, (name, family, email, password, id, address, phone))
+    conn.commit()
+
