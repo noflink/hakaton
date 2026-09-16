55c91b97f06ef6a80d498b2cf9fb07f2c47c025d
yaeli <yaelilan2008@gmail.com>
2026-09-16 09:26:47 +0300

    working db

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
 
-def start_soldiers_db():
+def start_guests_db():
     text = """CREATE TABLE IF NOT EXISTS GUESTS (
     guest_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
@@ -28,6 +28,20 @@ def start_hosts_db():
     )"""
     cursor.execute(text)
 
+def start_posts_db():
+    text = """CREATE TABLE IF NOT EXISTS POSTS (
+    post_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    host_id TEXT,
+    host_name TEXT,
+    host_family TEXT,
+    city TEXT,
+    address TEXT,
+    spots INTEGER,
+    available INTEGER,
+    CONTENT TEXT
+    )"""
+    cursor.execute(text)
+
 
 def add_guest(name, family, email, password):  # can be changed into an object class (simpler probably)
     text = """INSERT INTO GUESTS (name, family_name, email, password)
@@ -44,3 +58,29 @@ def add_host(name, family, email, password, id, address, phone):
     cursor.execute(text, (name, family, email, password, id, address, phone))
     conn.commit()
 
+
+def add_post(host_id, host_name, host_family, city, address, spots, content):
+    text = """INSERT INTO POSTS (host_id, host_name, host_family, city, address, spots, available, content)
+    VALUES (?,?,?,?,?,?,?,?)"""
+    cursor.execute(text, (host_id, host_name, host_family, city, address, spots, spots, content))
+    conn.commit()
+
+def get_posts(city):
+    text = """SELECT post_id FROM POSTS WHERE city=?"""
+    cursor.execute(text, (city,))
+    ids = cursor.fetchall()
+    print(ids)
+
+    posts = []
+
+    text = """SELECT * FROM POSTS WHERE post_id=?"""
+    for id in ids:
+        cursor.execute(text, (id[0],))
+        posts.append(cursor.fetchone())
+
+    return posts
+
+def get_host_posts(host_id):
+    text = """SELECT * FROM POSTS WHERE host_id=?"""
+    cursor.execute(text, (host_id,))
+    return cursor.fetchall()
