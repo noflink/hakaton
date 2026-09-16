900165a734622c167e6f43841b13ac881f59b67b
yaeli <yaelilan2008@gmail.com>
2026-09-16 11:29:26 +0300

    s

--- database.py
+++ database.py
@@ -40,7 +40,8 @@ def start_posts_db():
     address TEXT,
     spots INTEGER,
     available INTEGER,
-    content TEXT
+    content TEXT,
+    date TEXT
     )"""
     cursor.execute(text)
 
@@ -74,20 +75,23 @@ def get_posts(city):
     ids = cursor.fetchall()
     print(ids)
 
-    posts = []
+    temp = []
 
     text = """SELECT * FROM POSTS WHERE post_id=?"""
     for id in ids:
         cursor.execute(text, (id[0],))
-        posts.append(cursor.fetchone())
+        temp.append(cursor.fetchone())
 
+    posts = turn_to_dict(temp)
     return posts
 
 
 def get_host_posts(host_id):
     text = """SELECT * FROM POSTS WHERE host_id=?"""
     cursor.execute(text, (host_id,))
-    return cursor.fetchall()
+    temp = cursor.fetchall()
+    posts = turn_to_dict(temp)
+    return posts
 
 
 def start():
@@ -112,3 +116,26 @@ def check_login(email, password, usertype):
         return False
     return True
 
+def get_all_posts():
+    text = f"SELECT * FROM POSTS"
+    cursor.execute(text)
+    temp = cursor.fetchall()
+    posts = turn_to_dict(temp)
+    return posts
+
+def turn_to_dict(posts):
+    res = []
+    for item in posts:
+        post = {"post_id": item[0],
+                "host_id": item[1],
+                "host_name": item[2],
+                "host_family": item[3],
+                "city": item[4],
+                "address": item[5],
+                "spots": item[6],
+                "available": item[7],
+                "content": item[8],
+                "date" : item[9]}
+        res.append(post)
+
+    return res
